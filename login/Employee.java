da3b5570f9d86fc442ed6c47659f7a558faa9d85
Nisha2215 <karthirajan0706@gmail.com>
2019-05-07 10:04:08 +0530

    mmm

---
+++ login/Employee.java
@@ -0,0 +1,13 @@
+package org.cts.test.login;
+
+public class Employee {
+public void empId() {
+	System.out.println("emp id is 123456789");
+}
+	
+   public void empName() {
+	System.out.println("emp name is  nisha");
+}
+ 
+  
+}
---
+++ sample/ICICIBank.java
@@ -0,0 +1,13 @@
+package org.sample;
+
+public class ICICIBank {
+
+	public void saving() {
+	System.out.println("6%");
+	}
+	
+	
+	public void deposit(int id) {
+		System.out.println("3%");
+	}
+}
